92231c950fa64e28da142b6161c94cb17f73c428
M. Fazri Nizar <anon6372098.id@gmail.com>
2025-05-11 18:12:14 +0700

    Feat: construct index

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+// index.js
+// This script sets up an Express server and WebSocket connection for the smart feeder device.
+
 const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+const deviceRoutes = require('./routes/deviceRoutes');
+const { setupWebSocket } = require('./websocket');
+
 const app = express();
+app.use(bodyParser.json());
+app.use('/api', deviceRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
-});
+const server = http.createServer(app);
+setupWebSocket(server);
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log('Server running on port 3000');
 });
